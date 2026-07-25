8a1991da8df9e2c696d746d90c2ffd4e17c2d1dc
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-07-26 00:03:38 +0530

    GCD of two numbers

---
+++ RECURSION/GCD_ofTwoNum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int gcd(int a , int b){
+    if(b == 0){
+        return a;
+    }
+    else{
+        return gcd(b , a%b);
+    }
+}
+int main(){
+    int num1 , num2;
+    cout<<"Enter the first number : ";
+    cin>>num1;
+    cout<<"Enter the second number : ";
+    cin>>num2;
+    cout<<"GCD of "<<num1<<" and "<<num2<<" is :"<<gcd(num1 , num2);
+}
