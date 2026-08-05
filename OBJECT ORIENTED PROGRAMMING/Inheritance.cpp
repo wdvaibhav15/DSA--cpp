5e7328ff4d880c8aa2cee30c9de9ac6f04106f81
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-08-05 17:06:32 +0530

    inheritance using access modifier

--- OBJECT ORIENTED PROGRAMMING/Inheritance.cpp
+++ OBJECT ORIENTED PROGRAMMING/Inheritance.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Animal {
-public:
-    void sound() {
-        cout << "Animal makes sound\n";
-    }
+class Human {
+    public:
+    string name;
+    int age;
+    int weight; 
 };
-
-class Dog : public Animal {
-public:
-    void bark() {
-        cout << "Dog barks\n";
-    }
+class Student : public Human {
+    public:
+    int roll_number;
+    int fees;
 };
-
 int main() {
-    Dog d;
+   Student A;
+   A.roll_number = 1;
+   
 
-    d.sound();
-    d.bark();
 }
