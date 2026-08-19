c86bab9686e2f0f6a794de773bf5729d2da3ba28
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-08-19 16:46:58 +0530

    loop detection using slow or fast pointer

--- LINKED LIST/CIRCULAR LINKED LIST/detect_loop.cpp
+++ LINKED LIST/CIRCULAR LINKED LIST/detect_loop.cpp
@@ -1,71 +1,4 @@
-// #include <iostream>
-// #include <unordered_map>
-// using namespace std;
-
-// class node {
-// public:
-//     int data;
-//     node* next;
-//     node(int value) {
-//         data = value;
-//         next = NULL;
-//     }
-// };
-
-// int main() {
-//     node* head = NULL;
-//     node* tail = NULL;
-
-//     int n;
-//     cout << "Enter number of nodes: ";
-//     cin >> n;
-
-//     cout << "Enter elements: ";
-//     // Create singly linked list
-//     for (int i = 0; i < n; i++) {
-
-//         int value;
-//         cin >> value;
-//         node* temp = new node(value);
-//         if (head == NULL) {
-//             head = temp;
-//             tail = temp;
-//         }
-//         else {
-//             tail->next = temp;
-//             tail = temp;
-//         }
-//     }
-
-//     if (head != NULL && head->next != NULL) {
-//         tail->next = head->next;
-//     }
-    
-//     unordered_map<node*, bool> visited;
-//     node* current = head;
-//     bool loopFound = false;
-//     while (current != NULL) {
-//         if (visited[current] == true) {
-//             loopFound = true;
-//             break;
-//         }
-//         visited[current] = true;
-//         current = current->next;
-//     }
-
-//     if (loopFound) {
-//         cout << "\nLoop is present in the linked list.";
-//     }
-//     else {
-//         cout << "\nLoop is not present in the linked list.";
-//     }
-
-//     return 0;
-
-// }
-
 #include <iostream>
-#include <unordered_map>
 using namespace std;
 
 class node {
@@ -77,11 +10,22 @@ public:
         next = NULL;
     }
 };
+
 int main() {
     node* head = NULL;
     node* tail = NULL;
-    for (int i = 1; i <= 10; i++) {
-        node* temp = new node(i);
+
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    // Create circular linked list
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        node* temp = new node(value);
+        // First node
         if (head == NULL) {
             head = temp;
             tail = temp;
@@ -91,26 +35,20 @@ int main() {
             tail = temp;
         }
     }
-
-    node* node4 = head;
-    while (node4->data != 4) {
-        node4 = node4->next;
+    if (head != NULL) {
+        tail->next = head;
     }
-    tail->next = node4;
-    unordered_map<node*, bool> visited;
-    node* current = head;
-    while (current != NULL) {
-        if (visited[current] == true) {
-            cout << "Loop is present." << endl;
-            cout << "Loop starts at node: "
-                 << current->data << endl;
-            break;
+
+    node *slow = head;
+    node *fast = head;
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            cout << "Contains loop." << endl;
+            return 0;
         }
-
-        cout << "Visiting node: " << current->data << endl;
-        visited[current] = true;
-        current = current->next;
     }
-
+    cout << "Containing not a loop." << endl;
     return 0;
 }
