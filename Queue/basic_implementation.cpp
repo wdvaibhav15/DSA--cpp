ea34f6119497ef0d65ada81155efb80ef9e2507d
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-09-11 10:46:34 +0530

    pushb ,pop , front, in circular queue

--- Queue/basic_implementation.cpp
+++ Queue/basic_implementation.cpp
@@ -1,72 +1,85 @@
 #include<iostream>
 using namespace std;
-class queue{
-    int *arr;
-    int front;
+class queue {
+    int * arr;
+    int front ;
     int rear;
     int size;
     public:
-        queue(int n){
-            arr = new int[n];
-            front = -1;
-            rear = -1;
-            size = n;
+    queue(int n){
+        arr = new int [n];
+        front = -1;
+        rear= -1 ;
+        size = n;
+    }
+
+    // isfull function
+    bool isfull(){
+        if ((rear + 1) % size == front){
+            return true;
         }
-        // Function to add an element to the queue
-        void push(int x){
-            if(rear == size - 1){
-                cout<<"overflow"<<endl;
-                return;
-            }
-            if(front == -1){
-                front = 0;
-                arr[rear] = x;
-                cout<<arr[rear]<<" pushed into the queue"<<endl;
-            }else{
-                rear = rear + 1;
-                arr[rear] = x;
-                cout<<arr[rear]<<" pushed into the queue"<<endl;
-            }
+        else{
+            return false;
         }
-        // Function to remove an element from the queue
-        void pop(){
-            if(rear == -1 || front > rear){
-                cout<<"underflow"<<endl;
-                return;
-            }else if(front == rear){
-                front = -1;
-                rear = -1;
-            }else{
-                front = front + 1;
-                cout<<arr[front - 1]<<" Element popped from the queue"<<endl;
-            }
+    }
+
+    // isempty function
+     bool isempty(){
+        if( front == -1 || rear == -1){
+            return true;
         }
-        // Function to get the front element of the queue
-        int first(){
-            if(rear == -1 || front > rear){
-                cout<<"underflow"<<endl;
-                return -1;
-            }else{
-                cout<<arr[front]<<" is the front element of the queue"<<endl;
-                return arr[front];
-            }
+        else{
+            return false;
         }
-        //isEmpty function
-        bool isEmpty(){
-            if(rear == -1 || front > rear){
-                return true;
-            }else{
-                return false;
-            }
+     }
+
+    //push function
+    int push(int x){
+        if(isfull()){
+            cout<<"Overflow"<<endl;
+            return -1;
         }
-        // isFull function
-        bool isFull(){
-            if(rear == size - 1){
-                return true;
-            }else{
-                return false;
-            }
+        if(isempty()){
+            front = 0;
+            rear = 0;
+            arr[rear] = x;
+            cout<<"Element pushed: "<<x<<endl;
+            return 0;
         }
+        rear = (rear + 1) % size;
+        arr[rear] = x;
+        cout<<"Element pushed: "<<x<<endl;
+        return 0;
+    }
+
+    //pop function
+    int pop() {
+    if(isempty()) {
+        cout << "Underflow" << endl;
+        return -1;
+    }
+    int poppedValue = arr[front];
+    if(front == rear) {
+        front = -1;
+        rear = -1;
+    } 
+    else {
+        front = (front + 1) % size;
+    }
+    
+    cout << "Element popped: " << poppedValue << endl;
+    return poppedValue;
+}
+
+    // front function
+    int getfront(){
+        if(isempty()){
+            cout<<"Queue is empty"<<endl;
+            return -1;
+        }
+        cout<<"Front element is: "<<arr[front]<<endl;
+        return arr[front];
+    }
 };
 int main(){
     queue q(5);
@@ -77,7 +90,11 @@ int main(){
     q.push(5);
     q.push(6);
     q.pop();
-    cout<<q.first();
-    cout<<q.isEmpty();
-    cout<<q.isFull();
+    q.pop();
+    q.getfront();
+    q.push(6);
+    q.push(7);
+    q.push(8);
+    q.getfront();
+    return 0;
 }
